e563d62d1a185be0b5a6419ecf90aca9499d7217
Julien Biezemans <jb@jbpros.com>
2014-09-24 13:07:50 +0100

    Two scenarios passing

--- features/hear_shout.feature
+++ features/hear_shout.feature
@@ -1,5 +1,17 @@
 Feature: Hear Shout
 
-  A description goes here...
+  Rules
+  =====
+  - Must be within 500m
+  - Must be in range at time of shout
+  - No account needed to hear shouts
 
-  Scenario: ????
+  Scenario: Gertrude hears Bob's nearby coffee shout
+    Given Gertrude is 499 m away from Bob
+    When Bob shouts "coffee promo"
+    Then Gertrude hears "coffee promo"
+
+  Scenario: Gertrude hears Bob's nearby chocolate shout
+    Given Gertrude is 499 m away from Bob
+    When Bob shouts "free chocolate"
+    Then Gertrude hears "free chocolate"
--- lib/shouty.rb
+++ lib/shouty.rb
@@ -1,1 +1,43 @@
 # This is where we'll write the code of our Shouty app
+
+class Shouty
+  def initialize
+    @people = []
+  end
+
+  def person(name)
+    person = Person.new(self)
+    @people << person
+    person
+  end
+
+  def broadcast(message)
+    @people.each do |person|
+      person.receive(message)
+    end
+  end
+end
+
+class Person
+
+  def initialize(shouty)
+    @shouty = shouty
+    @received_messages = []
+  end
+
+  def position=(coordinates)
+  end
+
+  def shout(message)
+    @shouty.broadcast(message)
+  end
+
+  def receive(message)
+    @received_messages << message
+  end
+
+
+  def received_messages
+    @received_messages
+  end
+end
