9d35d93386df31f3c242ebc1d40926c2c18b8fc4
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2014-11-07 11:11:05 -0500

    Make them pending

--- web_steps/shout_steps.rb
+++ web_steps/shout_steps.rb
@@ -9,36 +9,21 @@ Before do
 end
 
 Given(/^Lucy is (\d+)ft away from Sean$/) do |distance|
-  @sean.visit('/?name=Sean&location=0')
-  @lucy.visit('/?name=Lucy&location=' + distance)
+  pending
 end
 
 When(/^Sean shouts "(.*?)"$/) do |message|
-  @sean.fill_in('Message', with: message)
-  @sean.click_button('Shout!')
-  @seans_message = message
+  pending
 end
 
 Then(/^Lucy should not hear Sean's shout$/) do
-  @lucy.visit(@lucy.current_url)
-  last_heard_message = @lucy.find('#last_heard_message').text rescue nil
-  if last_heard_message == @seans_message
-    raise "Lucy heard Sean's message (but we didn't expect her to)"
-  end
+  pending
 end
 
 Then(/^Lucy should hear Sean's shout$/) do
-  @lucy.visit(@lucy.current_url)
-  last_heard_message = @lucy.find('#last_heard_message').text rescue nil
-  if last_heard_message != @seans_message
-    raise "Lucy heard Sean's message (but we didn't expect her to)"
-  end
+  pending
 end
 
 Then(/^Sean should not hear himself$/) do
-  @sean.visit(@sean.current_url)
-  last_heard_message = @sean.find('#last_heard_message').text rescue nil
-  if last_heard_message == @seans_message
-    raise "Sean heard himself - that's bad"
-  end
+  pending
 end
