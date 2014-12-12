7a1a8e29f0e926fac076f89acb873b61edecc431
Julien Biezemans <jb@jbpros.com>
2014-12-12 12:54:35 +0000

    Basic shouting!

--- features/hear_shout.feature
+++ features/hear_shout.feature
@@ -1,5 +1,14 @@
 Feature: Hear Shout
 
-  A description goes here...
+  Personas:
+    * Lucy, the listener
+    * Sean, the shouter
 
-  Scenario: ????
+  Business rules:
+    * A shout is heard by listeners less than 1000ft from the shouter
+    * A shout is 140 characters or less
+
+  Scenario: Lucy hears Sean's shout
+    Given Lucy is 999ft away from Sean
+    When Sean shouts "10% off shoes today!"
+    Then Lucy hears "10% off shoes today!"
--- lib/shouty.rb
+++ lib/shouty.rb
@@ -1,1 +1,32 @@
-# This is where we'll write the code of our Shouty app
+class Network
+  def initialize
+    @people = []
+  end
+
+  def log_in(person)
+    @people << person
+  end
+
+  def broadcast(content)
+    @people.each do |person|
+      person.hear(content)
+    end
+  end
+end
+
+class Person
+  attr_reader :last_heard_shout
+
+  def initialize(network)
+    @network = network
+    @network.log_in(self)
+  end
+
+  def shout(content)
+    @network.broadcast(content)
+  end
+
+  def hear(content)
+    @last_heard_shout = content
+  end
+end
