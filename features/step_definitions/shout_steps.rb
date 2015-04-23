ad1dba2218b78c0e592192aa00df84736227742b
Julien Biezemans <jb@jbpros.com>
2015-04-22 21:30:36 -0700

    Initialize network in Before hook

--- features/step_definitions/shout_steps.rb
+++ features/step_definitions/shout_steps.rb
@@ -1,19 +1,21 @@
 require 'shouty'
 
+Before do
+  @network = Network.new
+end
+
 Given(/^Lucy is within Sean's range$/) do
-  network = Network.new
   sean_position = 0
   lucy_position = 1000
-  @sean = Person.new(network, sean_position)
-  @lucy = Person.new(network, lucy_position)
+  @sean = Person.new(@network, sean_position)
+  @lucy = Person.new(@network, lucy_position)
 end
 
 Given(/^Lucy is out of Sean's range$/) do
-  network = Network.new
   sean_position = 0
   lucy_position = 1001
-  @sean = Person.new(network, sean_position)
-  @lucy = Person.new(network, lucy_position)
+  @sean = Person.new(@network, sean_position)
+  @lucy = Person.new(@network, lucy_position)
 end
 
 When(/^Sean shouts(?: a short enough message)?$/) do
