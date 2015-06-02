448cd2063bf0500381127c1291baac13240e625a
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-06-02 15:11:20 +0100

    Split into 3 classes

--- lib/shouty.rb
+++ lib/shouty.rb
@@ -1,4 +1,5 @@
-require 'haversine'
+require 'message_service'
+require 'person'
 
 class Shouty
   def initialize
@@ -26,46 +27,5 @@ class Shouty
   def messages_heard_by(person_name)
     person = @people[person_name]
     person.messages_heard
-  end
-end
-
-class MessageService
-  def initialize(shouty)
-    @shouty = shouty
-  end
-
-  def broadcast(message, shouting_person)
-    @shouty.people.each do |receiving_person|
-      if within_range?(receiving_person.geo_location, shouting_person.geo_location)
-        receiving_person.hear(message)
-      end
-    end
-  end
-
-  def within_range?(geo_location_a, geo_location_b)
-    distance = Haversine.distance(geo_location_a, geo_location_b)
-    distance.to_meters <= 1000
-  end
-end
-
-class Person
-  attr_accessor :name
-  attr_accessor :geo_location
-
-  def initialize(message_service)
-    @message_service = message_service
-    @messages_heard = []
-  end
-
-  def shout(message)
-    @message_service.broadcast(message, self)
-  end
-
-  def messages_heard
-    @messages_heard
-  end
-
-  def hear(message)
-    @messages_heard.push(message)
   end
 end
