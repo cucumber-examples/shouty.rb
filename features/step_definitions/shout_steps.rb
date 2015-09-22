9ad37b6e8653ae0c8331510360a2b1b4a3c5f6f4
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-09-22 10:32:51 +0100

    Start working on 2nd step

--- features/step_definitions/shout_steps.rb
+++ features/step_definitions/shout_steps.rb
@@ -1,16 +1,19 @@
 require 'shouty'
 
+Before do
+  @shouty = Shouty.new
+end
+
 Given(/^Gush's location is more than (\d+)km from Elliott$/) do |distance|
   heathrow = [51.4716341,-0.4887383]
+  sky      = [51.4850955,-0.3223852]
 
-  shouty = Shouty.new
-
-  shouty.person_is_in("Gush", heathrow)
-  shouty.person_is_in("Elliott", sky)
+  @shouty.person_is_in("Gush", heathrow)
+  @shouty.person_is_in("Elliott", sky)
 end
 
 When(/^Elliott sends a message$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  @shouty.send_message("Elliott", message)
 end
 
 Then(/^Gush shouldn't receive a message from Elliott$/) do
--- lib/shouty.rb
+++ lib/shouty.rb
@@ -1,3 +1,5 @@
 # This is where we'll write the code of our Shouty app
 class Shouty
+  def person_is_in(user_name, geo_location)
+  end
 end
